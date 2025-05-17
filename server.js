4cf39550bf59bad69e4be5fddd6e96458ca0bca7
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2025-05-17 20:22:02 +0530

    post connection with author and data showed

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 async function main() {
-  const post = await prisma.post.create({
-    data: {
-      title: "Hello World",
-      body: "This is my first post",
-      author: {
-        connect: {
-          id: "68289f82a7324efd6121a9d9",
-        },
-      },
+  const post = await prisma.post.findMany({
+    include: {
+      author: true,
     },
   });
   console.log(post);
