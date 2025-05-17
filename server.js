f49bacee48480f6a1e91547e63ab5c8474d478db
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2025-05-17 20:19:03 +0530

    author id added in post data

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 async function main() {
-  const author = await prisma.author.create({
+  const post = await prisma.post.create({
     data: {
-      name: "John Doe",
+      title: "Hello World",
+      body: "This is my first post",
+      author: {
+        connect: {
+          id: "68289f82a7324efd6121a9d9",
+        },
+      },
     },
   });
-  console.log(author);
+  console.log(post);
+
+  //   const author = await prisma.author.create({
+  //     data: {
+  //       name: "John Doe",
+  //     },
+  //   });
+  //   console.log(author);
 }
+
 main();
